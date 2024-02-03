26d6f500325e9dc435c3a2e5877dba613af8ab5a
Nermin <sehicnermin97@gmail.com>
2024-02-03 18:39:40 +1100

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-cursive)", ...fontFamily.sans],
+      },
+      colors: {
+        pink: {
+          DEFAULT: "var(--pink)",
+          light: "var(--light-pink)",
+          dark: "var(--dark-pink)",
+        },
+        offWhite: "var(--off-white)",
+        foreground: "var(--foreground)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [],
 };
