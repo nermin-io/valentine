6641f0248d34ec255bb2ba76f5db311f34daae85
Nermin <sehicnermin97@gmail.com>
2024-02-03 20:31:50 +1100

    add success message

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,51 +1,17 @@
-import { HeartIcon } from "./icons/HeartIcon.tsx";
-import { Button } from "./Button.tsx";
 import { useState } from "react";
-
-const responses = [
-  "No",
-  "Are you sure?",
-  "Really sure?",
-  "Think again",
-  "Last chance",
-  "Surely not?",
-  "You might regret this",
-  "Give it another thought",
-  "Are you absolutely certain?",
-  "This could be a mistake!",
-  "Have a heart!",
-  "Don't be so cold!",
-  "Change of heart?",
-  "Wouldn't you reconsider?",
-  "Is that your final answer?",
-  "You're breaking my heart :(",
-];
+import { Question } from "./Question.tsx";
+import { SuccessMessage } from "./SuccessMessage.tsx";
 
 export function Card() {
-  const [clickCount, setClickCount] = useState(0);
-
-  const response = responses[Math.min(clickCount, responses.length - 1)];
-  const scale = Math.min(1 + clickCount / 10, 3);
+  const [success, setSuccess] = useState(false);
 
   return (
     <div className="bg-pink-light w-[600px] h-[600px] border-8 p-8 border-offWhite flex flex-col items-center justify-between">
-      <div className="flex flex-col items-center gap-10">
-        <h1 className="text-5xl text-center">
-          Leah, will you be my valentine?
-        </h1>
-        <HeartIcon height={150} width={150} />
-      </div>
-      <div className="flex flex-row gap-4">
-        <Button variant="primary" style={{ transform: `scale(${scale})` }}>
-          Yes
-        </Button>
-        <Button
-          variant="secondary"
-          onClick={() => setClickCount(clickCount + 1)}
-        >
-          {response}
-        </Button>
-      </div>
+      {success ? (
+        <SuccessMessage />
+      ) : (
+        <Question onSuccess={() => setSuccess(true)} />
+      )}
     </div>
   );
 }
